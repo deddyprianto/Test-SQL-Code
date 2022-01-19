64f6f0e4d975284fb2be3a6e8bca9b847bca87b5
deddyprianto <haicoders12@gmail.com>
2022-01-19 22:18:02 +0700

    add a sql injection

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
-
 const sqlite3 = require("sqlite3");
 
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database(":memory:");
 
 //Create users table
 
-let sql = "CREATE TABLE IF NOT EXISTS `users` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , `email` VARCHAR(100) NOT NULL , `name` VARCHAR(240) NOT NULL)";
+let sql =
+  "CREATE TABLE IF NOT EXISTS `users` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , `email` VARCHAR(100) NOT NULL , `name` VARCHAR(240) NOT NULL)";
 
-db.run(sql)
-
-
+db.run(sql);
 
 module.exports = db;
